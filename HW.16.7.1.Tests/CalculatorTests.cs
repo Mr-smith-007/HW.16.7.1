6bb670af1ed780d3158c5e27b783c54d7bcead83
Repnikov.A <repnikov.a@florcom.ru>
2023-10-12 15:05:21 +0300

    На проверку

---
+++ HW.16.7.1.Tests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._16._7._1
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(10, 2), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(10, 2), Is.EqualTo(8));
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(10, 2), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(10, 2), Is.EqualTo(5));
+        }
+
+    }
+
+}
